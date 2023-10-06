e2a790e8a354968f56bf27934b6c38473da4416d
em-tirupati <kishore.kumar@eminds.ai>
2023-10-06 14:38:10 +0530

    + Codeathon Core Java Problems with Solutions

--- codeathon/Codeathon06_Kishore.java
+++ codeathon/Codeathon06_Kishore.java
@@ -7,10 +7,8 @@
 package codeathon;
 
 import java.util.*;
-public class Codeathon06_Kishore
-{
-    public static void main(String[] args)
-    {
+public class Codeathon06_Kishore {
+    public static void main(String[] args) {
         List<String> list1 = new ArrayList<>();
         List<String> list2 = new ArrayList<>();
 
@@ -19,38 +17,36 @@ public class Codeathon06_Kishore
 
         System.out.println("List 1");
         for (int i = 0; i < n; i++) {    // Storing first list of values
-            list1.add( sc.next() );
+            list1.add(sc.next());
         }
 
         System.out.println("List 2");
         for (int i = 0; i < n; i++) {    // Storing second list of values
-            list2.add( sc.next() );
+            list2.add(sc.next());
         }
 
         // storing matching fruits to resultMap and displaying final output
         Map<String, List<String>> resultMap = new HashMap<>();
-        for (String text : list1)
-        {
-            List<String> matchingFruits = findMatchingFruits(text, list2);
+        for (String text : list1) {
+            List<String> matchingFruits = Matching.findMatchingFruits(text, list2);
             resultMap.put(text, matchingFruits);
         }
-        for (String list1String : list1)
-        {
+        for (String list1String : list1) {
             List<String> matchingFruits = resultMap.get(list1String);
 
             System.out.print(list1String + ": ");
             if (matchingFruits.isEmpty()) {
                 System.out.println("[No Fruit]");
-            }
-            else
-            {
+            } else {
                 System.out.println(String.join(", ", matchingFruits));
             }
         }
     }
-
+}
+class Matching
+{
     // method for finding matched fruits
-    private static List<String> findMatchingFruits(String numberString, List<String> list2) {
+    public static List<String> findMatchingFruits(String numberString, List<String> list2) {
         char alphabet = findAlphabetCharacter(numberString);
         List<String> matchingFruits = new ArrayList<>();
 
